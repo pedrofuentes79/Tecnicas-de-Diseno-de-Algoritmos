b5cbab6ad17df7eb8f8359c7a28575e1afa830c2
Flia Malenchini <fliamalenchini@MacBook-Pro-5.local>
2024-03-30 09:22:31 -0300

    finished it. python. shitty complexity. will make it better?

--- entregables/maradona/main.py
+++ entregables/maradona/main.py
@@ -7,78 +7,33 @@ def main():
         for j in range(10):
             players.append(input().split(" "))
 
-        global bestAttackers, bestDefenders, bestAttackSum, bestDefenseSum, attackers, defenders
-        bestAttackSum = 0
-        bestDefenseSum = 0
-        bestAttackers = []
-        bestDefenders = []
-        attackers = []
-        defenders = []
+        # sort players by attack, defense and earlier lexicographical order
+        sorted_players = radix(players)
+
+        # get the sorted version of this, meaning the top 5 players, get them in sorted order, as well as the other
+        bestAttackers = sorted(sorted_players[:5], key=lambda x: x[0])
+        bestDefenders = sorted(sorted_players[5:], key=lambda x: x[0])
         
-        backtrack(players, attackers, defenders, 0, bestAttackSum, bestDefenseSum, bestAttackers, bestDefenders)
-
-        # sort the answer
-        bestAttackers.sort()
-        bestDefenders.sort()
-
+        
         # print the answer
         print("Case #{}:".format(i+1))
-        print(bestAttackers)
-        print(bestDefenders)
+        output_attackers = '('
+        for i in range(len(bestAttackers)):
+            output_attackers += bestAttackers[i][0] + ', ' if i != len(bestAttackers)-1 else bestAttackers[i][0]
+        print(output_attackers + ')')
+        
+        output_defenders = '('
+        for i in range(len(bestDefenders)):
+            output_defenders += bestDefenders[i][0] + ', ' if i != len(bestDefenders)-1 else bestDefenders[i][0]
+        print(output_defenders + ')')
+        
+def radix(players):
+    # sort by attack
+    players = sorted(players, key=lambda x: x[0])
+    players = sorted(players, key=lambda x: x[2])
+    players = sorted(players, key=lambda x: x[1], reverse=True)
+    return players
 
-
-def backtrack(players, attackers, defenders, index, bestAttackSum, bestDefenseSum, bestAttackers, bestDefenders):
-
-    attackSum, defenseSum = 0, 0
-    for player in attackers:
-        attackSum += int(player[1])
-
-    for player in defenders:
-        defenseSum += int(player[2])
-    
-    # caso base: ya se recorrieron todos los jugadores
-    if index == len(players):
-        if attackSum > bestAttackSum or (attackSum == bestAttackSum and defenseSum > bestDefenseSum):
-            # reemplazo
-            print("Reemplazo")
-            bestAttackSum = attackSum
-            bestDefenseSum = defenseSum
-            bestAttackers = [player[0] for player in attackers]
-            bestDefenders = [player[0] for player in defenders]
-            print(bestAttackers, bestAttackSum)
-            print(bestDefenders, bestDefenseSum)
-
-        elif (attackSum == bestAttackSum and defenseSum == bestDefenseSum):
-            # reemplazar solamente cuando los nombres de los jugadores sean menores
-            # hacer una copia de los nombres de los jugadores, ordenarla y comparar
-            attackersCopy = sorted(attackers, key=lambda x: x[0])
-            bestAttackersCopy = sorted(bestAttackers, key=lambda x: x[0])
-            attackersCopy = [player[0] for player in attackersCopy]
-            bestAttackersCopy = [player[0] for player in bestAttackersCopy]
-
-            if attackersCopy < bestAttackersCopy:
-                bestAttackSum = attackSum
-                bestDefenseSum = defenseSum
-                bestAttackers = attackersCopy
-                bestDefenders = [player[0] for player in defenders]
-        else:
-            # no hay cambios
-            pass
-        return
-
-    # caso recursivo
-    if len(attackers) < 5:
-        attackers.append(players[index])
-        backtrack(players, attackers, defenders, index+1, bestAttackSum, bestDefenseSum, bestAttackers, bestDefenders)
-        attackers.pop()
-    else:
-        # agrego los jugadores que no están en attackers en defenders
-        defenders.append(players[index])
-        backtrack(players, attackers, defenders, index+1, bestAttackSum, bestDefenseSum, bestAttackers, bestDefenders)
-        defenders.pop()
-    
-
-
-
-if __name__ == "__main__":
+if __name__=='__main__':
     main()
+
